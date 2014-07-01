78784bd02a2087f50df2cc5e4073db6939ee90df
Margus Kerma <margus@amrek.net>
2014-07-01 18:56:42 +0300

    Initial Commit

---
+++ defaultbrowser/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  defaultbrowser
+//
+//  Created by margus kerma on 01/07/14.
+//  Copyright (c) 2014 margus kerma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
