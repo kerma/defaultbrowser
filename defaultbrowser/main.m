1d6101cffbff8599ade89b8eb6bd80594312b478
Stuart Ryan <stuart@ryan.email>
2015-08-07 17:37:36 +1000

    Created new command line parameters
    Refactored some code and enabled new command line parameter mode for -status
    Two options: available and current
    
    Intended for consuption by other applications.
    
    Signed-off-by: Stuart Ryan <stuart@ryan.email>

--- defaultbrowser/main.m
+++ defaultbrowser/main.m
@@ -8,35 +8,47 @@
 
 int main(int argc, const char * argv[])
 {
-
+    
     @autoreleasepool {
+        // we're interested in things which can handle http/https
+        NSArray *urlschemerefs = [[NSArray alloc] initWithObjects:@"http", @"https", nil];
+        
+        // lets figure out which handlers are available
+        NSArray *HTTPHandlers = (__bridge NSArray *) LSCopyAllHandlersForURLScheme(
+                                                                                   (__bridge CFStringRef)([urlschemerefs objectAtIndex:0]));
+        NSMutableDictionary *handlers = [NSMutableDictionary dictionary];
+        for (int i = 0; i < [HTTPHandlers count]; i++) {
+            NSString *split = [HTTPHandlers objectAtIndex:i];
+            NSArray *parts = [split componentsSeparatedByString:@"."];
+            [handlers setObject:split  forKey:[[parts lastObject] lowercaseString]];
+        }
+        
+        // what is our current handler?
+        NSString *currentHandler = (__bridge NSString *) LSCopyDefaultHandlerForURLScheme(
+                                                                                          (__bridge CFStringRef)([urlschemerefs objectAtIndex:0]));
+        
+        currentHandler = [[currentHandler componentsSeparatedByString:@"."] lastObject];
         
         // handler command line -set argument
         NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
         NSString *set = [args stringForKey:@"set"];
+        NSString *status = [args stringForKey:@"status"];
         
-        // we're interested in things which can handle http/https
-        NSArray *urlschemerefs = [[NSArray alloc] initWithObjects:@"http", @"https", nil];
-
-        if (set == nil) {
-            // what is our current handler?
-            NSString *currentHandler = (__bridge NSString *) LSCopyDefaultHandlerForURLScheme(
-                                            (__bridge CFStringRef)([urlschemerefs objectAtIndex:0]));
-            
-            currentHandler = [[currentHandler componentsSeparatedByString:@"."] lastObject];
+        if ([[status lowercaseString]  isEqual: @"available"] ) {
+            //list current available handlers
+            for (NSString *key in handlers) {
+                printf("%s\n", [key cStringUsingEncoding:NSUTF8StringEncoding]);
+            }
+            return 0;
+        } else if ([[status lowercaseString]  isEqual: @"current"] ) {
+            //output current default browser name
+            printf("%s\n", [currentHandler cStringUsingEncoding:NSUTF8StringEncoding]);
+            return 0;
+        } else if (set == nil) {
+            //assume default output
             printf("Current: %s\n\n", [currentHandler cStringUsingEncoding:NSUTF8StringEncoding]);
             printf("Use -set <browser> to set a new default HTTP handler\n");
         } else {
-            // lets figure out which handlers are available
-            NSArray *HTTPHandlers = (__bridge NSArray *) LSCopyAllHandlersForURLScheme(
-                                                            (__bridge CFStringRef)([urlschemerefs objectAtIndex:0]));
-            NSMutableDictionary *handlers = [NSMutableDictionary dictionary];
-            for (int i = 0; i < [HTTPHandlers count]; i++) {
-                NSString *split = [HTTPHandlers objectAtIndex:i];
-                NSArray *parts = [split componentsSeparatedByString:@"."];
-                [handlers setObject:split  forKey:[[parts lastObject] lowercaseString]];
-            }
-            
             // set a new default
             if ([handlers valueForKey:[set lowercaseString]] != nil) {
                 CFStringRef newHandler = (__bridge CFStringRef)([handlers valueForKey:[set lowercaseString]]);
