b8a83857f01492f7c14a20e4ead0dff770d014dc
Ildar Sagdejev <specious@gmail.com>
2017-12-30 23:13:44 -0500

    Proper string handling and well-defined distinction between application names and application bundle identifiers

--- src/main.m
+++ src/main.m
@@ -6,6 +6,10 @@
 #import <Foundation/Foundation.h>
 #import <ApplicationServices/ApplicationServices.h>
 
+NSString* app_name_from_bundle_id(NSString *app_bundle_id) {
+    return [[app_bundle_id componentsSeparatedByString:@"."] lastObject];
+}
+
 NSMutableDictionary* get_http_handlers(NSArray *url_scheme_refs) {
     NSArray *handlers =
       (__bridge NSArray *) LSCopyAllHandlersForURLScheme(
@@ -16,8 +20,7 @@ NSMutableDictionary* get_http_handlers(NSArray *url_scheme_refs) {
 
     for (int i = 0; i < [handlers count]; i++) {
         NSString *handler = [handlers objectAtIndex:i];
-        NSArray  *parts   = [handler componentsSeparatedByString:@"."];
-        [dict setObject:handler forKey:[[parts lastObject] lowercaseString]];
+        [dict setObject:handler forKey:[app_name_from_bundle_id(handler) lowercaseString]];
     }
 
     return dict;
@@ -29,13 +32,11 @@ NSString* get_current_http_handler(NSArray *url_scheme_refs) {
             (__bridge CFStringRef) ([url_scheme_refs objectAtIndex:0])
         );
 
-    handler = [[handler componentsSeparatedByString:@"."] lastObject];
-
-    return handler;
+    return app_name_from_bundle_id(handler);
 }
 
 int main(int argc, const char *argv[]) {
-    const char *target = argv[1];
+    const char *target = (argc == 1) ? '\0' : argv[1];
 
     @autoreleasepool {
         NSArray *url_scheme_refs = [[NSArray alloc] initWithObjects:@"http", @"https", nil];
@@ -44,24 +45,24 @@ int main(int argc, const char *argv[]) {
         NSMutableDictionary *handlers = get_http_handlers(url_scheme_refs);
 
         // Get current HTTP handler
-        NSString *current_handler = get_current_http_handler(url_scheme_refs);
+        NSString *current_handler_name = get_current_http_handler(url_scheme_refs);
 
         if (target == '\0') {
             // List all HTTP handlers, marking the current one with a star
             for (NSString *key in handlers) {
-                char *mark = (key == current_handler) ? "* " : "  ";
-                printf("%s%s\n", mark, [key cStringUsingEncoding:NSUTF8StringEncoding]);
+                char *mark = [key isEqual:current_handler_name] ? "* " : "  ";
+                printf("%s%s\n", mark, [key UTF8String]);
             }
         } else {
-            NSString *target_handler_key = [NSString stringWithUTF8String:target];
+            NSString *target_handler_name = [NSString stringWithUTF8String:target];
 
-            if (target_handler_key == current_handler ) {
+            if ([target_handler_name isEqual:current_handler_name]) {
               printf("%s is already set as the default HTTP handler\n", target);
             } else {
-                NSString *target_handler = [handlers valueForKey:target_handler_key];
+                NSString *target_handler = [handlers valueForKey:target_handler_name];
 
                 if (target_handler != nil) {
-                    // Set new HTTP handler
+                    // Set new HTTP handler (HTTP and HTTPS separately)
                     for (NSString *url_scheme_ref in url_scheme_refs) {
                         LSSetDefaultHandlerForURLScheme(
                             (__bridge CFStringRef) url_scheme_ref,
